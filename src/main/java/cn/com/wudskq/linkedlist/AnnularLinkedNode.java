083a9abd579c53d7405e1208f6126cb9c0455fcf
chenfangchao <socrates11@foxmail.com>
2021-12-02 03:37:30 +0800

    数据结构-单向链表实现环形链表-约瑟夫出圈问题解决

---
+++ src/main/java/cn/com/wudskq/linkedlist/AnnularLinkedNode.java
@@ -0,0 +1,36 @@
+package cn.com.wudskq.linkedlist;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author chenfangchao
+ * @version 1.0.0
+ * @ClassName AnnularLinkedList.java
+ * @Description TODO 单向环形链表节点
+ * @createTime 2021年12月02日 01:59:00
+ */
+@Data
+public class AnnularLinkedNode implements Serializable {
+
+    private int id;
+
+    private String name;
+
+    private AnnularLinkedNode next;
+
+    public AnnularLinkedNode(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.next = null;
+    }
+
+    @Override
+    public String toString() {
+        return "AnnularLinkedNode{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
