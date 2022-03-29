620265d31abbfb44f515abbf431b63603c7cc5f8
chenfangchao <socrates11@foxmail.com>
2022-03-29 20:07:00 +0800

    [add]#二分法算法优化

--- ReadMe.md
+++ ReadMe.md
@@ -2073,11 +2073,72 @@ public static int[] selectSort(int[] array) {
     }
     ```
 
-    
+  - 二分法算法优化:
+
+    - 场景描述: 若给定的一个数组中,需要查找的数据在给定的数组中有两个及以上,输出该数组中符合条件的所有数据
+
+    - 核心思想: 二分法基本思想不变,需要变动的为找到目标数值之后,创建临时指针使其等于下标middle
+
+      根据middle分别向左扫描与向右扫描,找到符合条件的值并加入到list中去
+
+    - 核心代码:
+
+      ```java
+      
+      /**
+           * 思路分析: 在找到符合条件的middle的下标时
+           * 开始从符合的下标 middle 左边及右边找到符合条件的Index并加入到list
+           * @param arrays
+           * @param left
+           * @param right
+           * @param findValue
+           * @return
+           */
+      public List<Integer> binarySearch(int[] arrays, int left, int right, int findValue){
+        //数据
+        ArrayList<Integer> list = new ArrayList<>();
+        //进行排序
+        Arrays.sort(arrays);
+        //递归结束条件
+        //如果左下标大于了右下标||右下标小于左下标则证明找不到数据
+        if(left > right){
+          return null;
+        }
+        int middle = (left + right) /2;
+        int middleData = arrays[middle];
+        //向右递归
+        if(findValue > middleData){
+          return binarySearch(arrays,middle+1,right,findValue);
+        }else if(findValue < middleData){ //向左递归
+          return binarySearch(arrays,left,middle-1,findValue);
+        }else{ //直接返回
+          list.add(middle);
+          //向左扫描
+          int tempIndex = middle-1;
+          while (true){
+            if(tempIndex < 0 || arrays[tempIndex] != findValue){
+              break;
+            }
+            list.add(tempIndex);
+            tempIndex -= 1;
+          }
+          //向右扫描
+          tempIndex = middle+1;
+          while (true){
+            if(tempIndex > arrays.length-1 || arrays[tempIndex] != findValue){
+              break;
+            }
+            list.add(tempIndex);
+            tempIndex += 1;
+          }
+        }
+        return list;
+      }
+      ```
+
+      
 
   
-
-
 
 ### 10.引用
 
--- src/main/java/cn/com/wudskq/algorithm/serach/BinarySearch.java
+++ src/main/java/cn/com/wudskq/algorithm/serach/BinarySearch.java
@@ -1,7 +1,9 @@
 package cn.com.wudskq.algorithm.serach;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author chenfangchao
@@ -12,22 +14,63 @@ import java.util.Arrays;
  */
 public class BinarySearch {
 
-    static int[] arrays = {1,2,4,6,8,5,9,10,23,43,554,65};
+    static int[] arrays = {1,2,4,6,8,5,9,10,10,43,554,65};
 
     public static void main(String[] args) {
         BinarySearch binarySearch = new BinarySearch();
-        System.out.println(binarySearch.binarySearch1(arrays, 0, arrays.length - 1, 10));
+        System.out.println(binarySearch.binarySearch(arrays, 0, arrays.length - 1, 10));
 
     }
 
 
-    public int binarySearch(int[] arrays,int findValue){
+    /**
+     * 思路分析: 在找到符合条件的middle的下标时
+     * 开始从符合的下标 middle 左边及右边找到符合条件的Index并加入到list
+     * @param arrays
+     * @param left
+     * @param right
+     * @param findValue
+     * @return
+     */
+    public List<Integer> binarySearch(int[] arrays, int left, int right, int findValue){
+        //数据
+        ArrayList<Integer> list = new ArrayList<>();
         //进行排序
         Arrays.sort(arrays);
-        int left = 0,right = arrays.length-1;
-        int middle = (left + right)/2;
+        //递归结束条件
+        //如果左下标大于了右下标||右下标小于左下标则证明找不到数据
+        if(left > right){
+            return null;
+        }
+        int middle = (left + right) /2;
         int middleData = arrays[middle];
-        return -1;
+        //向右递归
+        if(findValue > middleData){
+            return binarySearch(arrays,middle+1,right,findValue);
+        }else if(findValue < middleData){ //向左递归
+            return binarySearch(arrays,left,middle-1,findValue);
+        }else{ //直接返回
+            list.add(middle);
+            //向左扫描
+            int tempIndex = middle-1;
+            while (true){
+                if(tempIndex < 0 || arrays[tempIndex] != findValue){
+                    break;
+                }
+                list.add(tempIndex);
+                tempIndex -= 1;
+            }
+            //向右扫描
+            tempIndex = middle+1;
+            while (true){
+                if(tempIndex > arrays.length-1 || arrays[tempIndex] != findValue){
+                    break;
+                }
+                list.add(tempIndex);
+                tempIndex += 1;
+            }
+        }
+        return list;
     }
 
     public int binarySearch1(int[] arrays, int left,int right,int findValue){
