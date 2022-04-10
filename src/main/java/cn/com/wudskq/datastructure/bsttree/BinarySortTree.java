277852a2029527d6918467cd2b0e6434a2b15d5b
chenfangchao <socrates11@foxmail.com>
2022-04-10 21:38:21 +0800

    [add]#二叉排序树添加

---
+++ src/main/java/cn/com/wudskq/datastructure/bsttree/BinarySortTree.java
@@ -0,0 +1,59 @@
+package cn.com.wudskq.datastructure.bsttree;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author chenfangchao
+ * @title: BinaySortTree
+ * @projectName structure-project
+ * @description: TODO 二叉排序树
+ * @date 2022/4/10 9:27 PM
+ */
+public class BinarySortTree {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    private BinarySortTreeNode root;
+
+    static int[] array = {7,2,2,10,12,5,1,9};
+    //内存结构图
+    /**
+     *        7
+     *    2      10
+     * 1    5  9    12
+     *
+     */
+
+    public static void main(String[] args) {
+        BinarySortTree binarySortTree = new BinarySortTree();
+        //添加数据
+        for (int a = 0; a < array.length; a++) {
+            BinarySortTreeNode node = new BinarySortTreeNode(array[a]);
+            binarySortTree.add(node);
+        }
+        //中序遍历
+        binarySortTree.midOrder();
+    }
+
+
+
+    //添加节点
+    public void  add(BinarySortTreeNode node){
+        if(root == null){
+            root = node;
+        }else {
+            root.add(node);
+        }
+    }
+
+    //中序遍历
+    public void  midOrder(){
+        if(root != null){
+            root.midOrder();
+        }else {
+            logger.info("current is empty!");
+        }
+    }
+}
+
---
+++ src/main/java/cn/com/wudskq/datastructure/bsttree/BinarySortTreeNode.java
@@ -0,0 +1,68 @@
+package cn.com.wudskq.datastructure.bsttree;
+
+import cn.com.wudskq.datastructure.tree.BinaryTreeNode;
+import com.alibaba.druid.sql.visitor.functions.Bin;
+import lombok.Data;
+
+/**
+ * @author chenfangchao
+ * @title: BinarySortTreeNode
+ * @projectName structure-project
+ * @description: TODO 二叉排序树节点
+ * @date 2022/4/10 9:16 PM
+ */
+@Data
+public class BinarySortTreeNode {
+
+    private int value;
+
+    private BinarySortTreeNode left;
+
+    private BinarySortTreeNode right;
+
+    public BinarySortTreeNode(int value) {
+        this.value = value;
+    }
+
+    //递归添加节点(需要满足二叉排序树约定)
+    public void  add(BinarySortTreeNode node){
+        if(node == null){
+            return;
+        }
+        //添加节点小于当前节点
+        //指针往左边挂
+        if(node.value < this.value){
+            if(this.left == null){
+                this.left = node;
+            }else {
+               this.left.add(node);
+            }
+        //节点大于当前节点 指针往右边挂
+        }else{
+            if(this.right == null){
+                this.right = node;
+            }else {
+                this.right.add(node);
+            }
+        }
+    }
+
+    //中序遍历
+    public void midOrder(){
+        if(this.left != null){
+            this.left.midOrder();
+        }
+        System.out.println(this);
+        if(this.right != null){
+            this.right.midOrder();
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "BinarySortTreeNode{" +
+                "value=" + value +
+                '}';
+    }
+}
